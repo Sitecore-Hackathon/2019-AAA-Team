65a6b77025a8d615a332fa1f5e0cfdbc373dffc5
a.paliakou <a.paliakou@brimit.com>
2019-03-02 13:08:24 -0800

    .

---
+++ src/Feature/Hackathon.AAATeam.Feature.Navigation/Models/BreadcrumsItems.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Hackathon.AAATeam.Feature.Navigation.Models
+{
+    public class BreadcrumsItems : Sitecore.Commerce.Core.Model
+    {
+        public List<BreadcrumbModel> Items { get; set; }
+        public BreadcrumsItems()
+        {
+            Items = new List<BreadcrumbModel>();
+        }
+
+        public BreadcrumsItems(List<BreadcrumbModel> items)
+        {
+            Items = items;
+        }
+    }
+}
--- src/Feature/Hackathon.AAATeam.Feature.Navigation/Pipelines/BizFxBreadcrumbPipeline.cs
+++ src/Feature/Hackathon.AAATeam.Feature.Navigation/Pipelines/BizFxBreadcrumbPipeline.cs
@@ -3,10 +3,11 @@ using Microsoft.Extensions.Logging;
 using Sitecore.Commerce.Core;
 using Sitecore.Commerce.EntityViews;
 using Sitecore.Framework.Pipelines;
+using System.Collections.Generic;
 
 namespace Hackathon.AAATeam.Feature.Navigation.Pipelines
 {
-    public class BizFxBreadcrumbPipeline : CommercePipeline<string, BreadcrumbModel>, IBizFxBreadcrumbPipeline, IPipeline<string, BreadcrumbModel, CommercePipelineExecutionContext>, IPipelineBlock<string, BreadcrumbModel, CommercePipelineExecutionContext>, IPipelineBlock, IPipeline
+    public class BizFxBreadcrumbPipeline : CommercePipeline<string, List<BreadcrumbModel>>, IBizFxBreadcrumbPipeline, IPipeline<string, List<BreadcrumbModel>, CommercePipelineExecutionContext>, IPipelineBlock<string, List<BreadcrumbModel>, CommercePipelineExecutionContext>, IPipelineBlock, IPipeline
     {
         public BizFxBreadcrumbPipeline(
           IPipelineConfiguration<IBizFxBreadcrumbPipeline> configuration,
